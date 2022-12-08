59c1750a1534dcee5d81fd78aad900bdbd4f8be1
lhj0954 <leehakjun111@naver.com>
2022-12-08 21:10:58 +0900

    2단계 진행 중

--- src/코딩테스트연습/level2/방금그곡.java
+++ src/코딩테스트연습/level2/방금그곡.java
@@ -5,7 +5,52 @@ import java.text.SimpleDateFormat;
 import java.text.ParseException;
 
 public class 방금그곡 {
-    public static int getTime (String time1, String time2) {
+    public String solution(String m,String[] musicinfos){
+        String answer = "(None)";
+        int maxtime = 0;
+        for(int i = 0; i<m.length();i++){
+            m = m.replace("A#","a");
+            m = m.replace("C#","c");
+            m = m.replace("D#","d");
+            m = m.replace("F#","f");
+            m = m.replace("G#","g");
+            m = m.replace("E#","e");
+        }
+
+        for(int i = 0; i< musicinfos.length;i++) {
+            String[] tmp = musicinfos[i].split(",");
+
+            tmp[3] = tmp[3].replace("A#","a");
+            tmp[3] = tmp[3].replace("C#","c");
+            tmp[3] = tmp[3].replace("D#","d");
+            tmp[3] = tmp[3].replace("F#","f");
+            tmp[3] = tmp[3].replace("G#","g");
+            tmp[3] = tmp[3].replace("E#","e");
+
+
+            String[] starttime = tmp[0].split(":");
+            String[] endtime = tmp[1].split(":");
+
+            int hour = Integer.parseInt(endtime[0]) - Integer.parseInt(starttime[0]);
+            int min = Integer.parseInt(endtime[1]) - Integer.parseInt(starttime[1]) + (hour * 60);
+
+            String meltmp = "";
+            for(int j = 0; j<min;j++){
+                meltmp += tmp[3].charAt(j%tmp[3].length());
+            }
+            //System.out.println(meltmp); 노래가 진행되는 동안의 음들
+            if(meltmp.contains(m)){
+                if(maxtime < meltmp.length()){
+                    maxtime = meltmp.length();
+                    answer = tmp[2];
+                }
+            }
+        }
+        return answer;
+    }
+
+}
+/*public static int getTime (String time1, String time2) {
         long differ = 0;
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
@@ -36,14 +81,14 @@ public class 방금그곡 {
         String answer = "(None)";
 
         String[] mList = toArray(m);
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new LinkedHashMap<>();
         for(String musicinfo : musicinfos) {
             String[] temp = musicinfo.split(",");
             int time = getTime(temp[0], temp[1]);
             String[] melody;
             if(temp[3].length() >= time) {
                 StringBuilder sb = new StringBuilder();
-                for(int i = 0; i < sb.length(); i++) {
+                for(int i = 0; i < time; i++) {
                     sb.append(temp[3].charAt(i));
                 }
                 melody = toArray(sb.toString());
@@ -69,16 +114,13 @@ public class 방금그곡 {
             }
         }
 
-        for(String key : map.keySet()) {
-            System.out.println(key + " : " + map.get(key));
+        List<String> list = new ArrayList<>(map.keySet());
+        list.sort((s1, s2) -> map.get(s2) - map.get(s1));
+        //list.sort(Comparator.comparingInt(map::get));
+
+        if(map.size() > 0) {
+            answer = list.get(0);
         }
 
         return answer;
-    }
-
-    public static void main(String[] args) {
-        String m = "ABCDEFG";
-        String[] mi = {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
-        solution(m, mi);
-    }
-}
+    }*/
